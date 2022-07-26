cabaa06ff553caab5d78ea438bb8dc3311d30e27
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-07-26 13:13:57 +0200

    hmmm

--- common/hooks/useMousePosition.tsx
+++ common/hooks/useMousePosition.tsx
@@ -1,20 +1,19 @@
 import { useState, useEffect } from 'react';
 
-
 // Inspiration for this snippet: Josh W. Comeau (https://www.joshwcomeau.com/snippets/react-hooks/use-mouse-position/)
 
 type MousePositionType = {
     x: number | null;
     y: number | null;
-}
+};
 
 const useMousePosition = () => {
-    const [
-        mousePosition,
-        setMousePosition
-    ] = useState<MousePositionType>({ x: null, y: null });
+    const [mousePosition, setMousePosition] = useState<MousePositionType>({
+        x: null,
+        y: null,
+    });
 
-    const [mouseOverButton, setMouseOverButton] = useState(false)
+    const [mouseOverButton, setMouseOverButton] = useState(false);
 
     useEffect(() => {
         const updateMousePosition = (ev: MouseEvent) => {
@@ -24,18 +23,22 @@ const useMousePosition = () => {
             setMousePosition({ x: 0, y: 0 });
         };
         const handleMouseOver = (e: MouseEvent) => {
-            if (e.target instanceof HTMLButtonElement || e.target instanceof HTMLAnchorElement || (e.target as HTMLObjectElement).id === 'button-text') {
+            if (
+                e.target instanceof HTMLButtonElement ||
+                e.target instanceof HTMLAnchorElement ||
+                (e.target as HTMLObjectElement).id === 'button-text'
+            ) {
                 setMouseOverButton(true);
                 return;
             }
             setMouseOverButton(false);
-        }
+        };
         window.addEventListener('mousemove', updateMousePosition);
-        window.addEventListener("mouseout", handleMouseLeave);
+        window.addEventListener('mouseout', handleMouseLeave);
         window.addEventListener('mouseover', handleMouseOver);
         return () => {
             window.removeEventListener('mousemove', updateMousePosition);
-            window.removeEventListener("mouseout", handleMouseLeave);
+            window.removeEventListener('mouseout', handleMouseLeave);
             window.removeEventListener('mouseover', handleMouseOver);
         };
     }, []);
